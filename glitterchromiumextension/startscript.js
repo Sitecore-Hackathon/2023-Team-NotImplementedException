32ff267f16214fcccb59e45ced84e06107987f3b
Carl Nielsen <carl@comfortdata.dk>
2023-03-04 21:57:28 +0100

    chrome extensions

--- glitterchromiumextension/startscript.js
+++ glitterchromiumextension/startscript.js
@@ -71,7 +71,12 @@ chrome.tabs.query({
       .then(injectionResults => {
         for (const frameResult of injectionResults) {
           const {frameId, result} = frameResult;
-          updateExtensionInterface(result.itemId, result.itemLanguage, result.itemVersion);
+            if(result && result.itemId != null && result.itemLanguage != null && result.itemVersion != null) {
+                updateExtensionInterface(result.itemId, result.itemLanguage, result.itemVersion);
+            } else {
+                var historyElement = document.getElementById("history");
+                historyElement.innerHTML = "No item information found";
+            }
         }
       });
 });
@@ -89,6 +94,7 @@ async function updateExtensionInterface(itemId, itemLanguage, itemVersion) {
 async function fetchApiData(itemId, itemLanguage, itemVersion) {
     let cleaneditemId = itemId.replace("{", "").replace("}", "");
     let url = "https://glitterbucket.localhost/item/" + cleaneditemId + "/version/" + itemVersion;
+    alert(url);
     let response = await fetch(url);
     let historydata = await response.json();
     return historydata[0].title;
