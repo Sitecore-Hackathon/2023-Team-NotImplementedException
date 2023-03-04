44166ea07392ae3c1ca3e23a844346b2a72b2bb8
Carl Nielsen <carl@comfortdata.dk>
2023-03-04 19:33:25 +0100

    fixed version for experienceeditor

--- glitterchromiumextension/startscript.js
+++ glitterchromiumextension/startscript.js
@@ -1,18 +1,21 @@
 
-function checkEditor() {
-    // check for content editor
-    
-}
 
+// Get item information from Sitecore
 function getCurrentItemInformation() {
     
     var itemId, itemLanguage, itemVersion, editor;
-
-    if(document.URL.indexOf("sitecore/shell/Applications/Content%20Editor.aspx") > -1) {
-        editor = "contenteditor";
-    } else {
-        editor = "experienceeditor";
+    
+    var determineSitecoreEditor = function() {
+        if(document.URL.indexOf("sitecore/shell/Applications/Content%20Editor.aspx") > -1) {
+            editor = "contenteditor";
+        } else {
+            editor = "experienceeditor";
+        }
+        return editor;
     }
+
+
+    var editor = determineSitecoreEditor();
 
     switch(editor) {
         case "experienceeditor":
@@ -21,7 +24,7 @@ function getCurrentItemInformation() {
             var itemVersionElement = document.getElementById("scVersion");
             
             if (itemIdElement != null && itemLanguageElement != null && itemVersionElement != null) {
-                ItemId = itemVersion.value;
+                itemId = itemIdElement.value;
                 itemLanguage = itemLanguageElement.value; 
                 itemVersion = itemVersionElement.value;
             }
@@ -32,30 +35,30 @@ function getCurrentItemInformation() {
             itemLanguage = params.get("sc_lang");
             itemVersion = params.get("sc_version");
 
+            
+            // if nothing was found, try to get the itemversion from the ribbon :S
+            if (itemVersion == null) {
+                var element = document.getElementById('scWebEditRibbon').contentWindow.document.querySelectorAll('[data-sc-id="PageEditBar"]');
+                if (element && element.length > 0) {
+                    itemVersion = element[0].getAttribute("data-sc-version");
+                }
+            }
+
             return {itemId, itemLanguage, itemVersion};
             
         case "contenteditor":
             // Experience editor
             var itemInfo = document.getElementById("__CurrentItem").value
-            //"sitecore://master/{6702D14A-E01D-4E32-B722-A2B30AE9BD1D}?lang=en&ver=1"
             itemId = itemInfo.substring(itemInfo.indexOf("{"), itemInfo.indexOf("}") + 1);
             itemLanguage = itemInfo.substring(itemInfo.indexOf("lang=") + 5, itemInfo.indexOf("&ver="));
             itemVersion = itemInfo.substring(itemInfo.indexOf("ver=") + 4);
-            return {itemId, itemLanguage, itemVersion};
+            return {itemId, itemLanguage, itemVersion}
     }
 
-    function queryApi(itemId, itemLanguage, itemVersion) {
-        fetch('https://jsonplaceholder.typicode.com/posts').then(function (response) {
-	        // The API call was successful!
-	        console.log('success!', response);
-        }).catch(function (err) {
-	        // There was an error
-	        console.warn('Something went wrong.', err);
-        });
-    }
-
+    
 }
 
+// extension startup script
 chrome.tabs.query({
     active: true,
     lastFocusedWindow: true
@@ -74,9 +77,17 @@ chrome.tabs.query({
 });
 
 
-function updateExtensionInterface(itemId, itemLanguage, itemVersion) {
+// update the extension gui with item information + glitter data
+async function updateExtensionInterface(itemId, itemLanguage, itemVersion) {
     var historyElement = document.getElementById("history");
-    historyElement.innerHTML = "ItemId: " + itemId + "<br/>Language: " + itemLanguage + "<br/>" + "<br/>Version: " + itemVersion + "<br/>";
+    var apiData = await fetchApiData(itemId, itemLanguage, itemVersion);
+    historyElement.innerHTML = "ItemId: " + itemId + "<br/>Language: " + itemLanguage + "<br/>" + "<br/>Version: " + itemVersion + "<br/>" + apiData;
 }
 
 
+// function to fetch data from the glitter API
+async function fetchApiData(itemId, itemLanguage, itemVersion) {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let historydata = await response.json();
+    return historydata[0].title;
+}
