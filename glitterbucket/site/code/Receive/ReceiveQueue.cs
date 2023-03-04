dc0b3058ede1b0f830f84a95fda66e4ae3e160ad
Jesper Balle <Jesper.Balle@valtech.com>
2023-03-05 00:31:21 +0100

    Log errors in glitterbucket

--- glitterbucket/site/code/Receive/ReceiveQueue.cs
+++ glitterbucket/site/code/Receive/ReceiveQueue.cs
@@ -32,13 +32,21 @@ namespace GlitterBucket.Receive
         {
             await foreach (var itm in _channel.ReadAllAsync(stoppingToken))
             {
-                var id = itm.SitecoreInstanceId;
-                var raw = itm.ReceivedData;
-                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(raw));
-                var model = await JsonSerializer.DeserializeAsync<SitecoreWebHookModel>(stream, cancellationToken: stoppingToken);
-                if (model == null) continue;
-                await _client.Add(id, model, raw);
-                _logger.LogInformation("Stored {EventName} from {SitecoreId}: {Raw}", model.EventName, id, raw);
+                try
+                {
+                    var id = itm.SitecoreInstanceId;
+                    var raw = itm.ReceivedData;
+                    using var stream = new MemoryStream(Encoding.UTF8.GetBytes(raw));
+                    var model = await JsonSerializer.DeserializeAsync<SitecoreWebHookModel>(stream,
+                        cancellationToken: stoppingToken);
+                    if (model == null) continue;
+                    await _client.Add(id, model, raw);
+                    _logger.LogInformation("Stored {EventName} from {SitecoreId}: {Raw}", model.EventName, id, raw);
+                }
+                catch (Exception exc)
+                {
+                    _logger.LogError(exc, "Could not process event {EventData} due to {Error}", itm.ReceivedData, exc.Message);
+                }
             }
         }
     }
